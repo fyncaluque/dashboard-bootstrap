8982bef264a2ca6f7964d6b32d5a8734e1b015e5
Fernando M. Yncaluque <fyncaluque@gmail.com>
2023-03-19 16:25:13 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,55 @@
+const ctx = document.getElementById('chart');
+
+  // new Chart(ctx, {
+  //   type: 'bar',
+  //   data: {
+  //     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  //     datasets: [{
+  //       label: '# of Votes',
+  //       data: [12, 19, 3, 5, 2, 3],
+  //       borderWidth: 1
+  //     }]
+  //   },
+  //   options: {
+  //     scales: {
+  //       y: {
+  //         beginAtZero: true
+  //       }
+  //     }
+  //   }
+  // });
+
+
+
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+      label: 'Looping tension',
+      data: [65, 59, 80, 81, 26, 55, 40],
+      // fill: true,
+      // borderColor: 'rgb(75, 192, 192)',
+    }]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {
+      animations: {
+        tension: {
+          duration: 1000,
+          easing: 'linear',
+          from: 1,
+          to: 0,
+          loop: true
+        }
+      },
+      scales: {
+        y: { // defining min and max so hiding the dataset does not change scale range
+          beginAtZero: true
+        }
+      }
+    }
+  };
+
+  new Chart(ctx,config);
